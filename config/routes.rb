128de6b438a711462fe95719cce35457f5e8adf4
mokahontas <alexisfelicilda@yahoo.com>
2016-06-15 02:54:31 -1000

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/login' => 'session#new', :as => 'login'
     post '/login' => 'session#create'
     delete '/login' => 'session#destroy', :as => 'logout'
-    root 'welcome#index', :as => 'home'
+    root 'session#new', :as => 'home'
 
   resources :session
   resources :reservations
