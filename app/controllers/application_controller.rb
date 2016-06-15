519076766f3f80b426780c2bfd80c144551f190a
onesc <onescyt@gmail.com>
2016-06-15 17:07:55 +1000

    only admins may create planes and flights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
     private
       def fetch_user
+        @planes = Plane.all
+        @flights = Flight.all
         @current_user = User.find_by(:id => session[:user_id]) if session[:user_id].present?
         session[:user_id] = nil unless @current_user.present?
       end
